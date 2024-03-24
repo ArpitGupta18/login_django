a7c637903c89bf1af874976fc9bb47515cf47731
ArpitGupta18 <123069437+ArpitGupta18@users.noreply.github.com>
2024-03-24 18:12:31 +0545

    sign-up page was redirected to sign-in page after filling in the form. The data of the form was saved in 'db.sqlite3'

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -7,6 +9,24 @@ def home(request):
 
 
 def sign_up(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm-password']
+
+        myuser = User.objects.create_user(username, email, password)
+        myuser.first_name = first_name
+        myuser.last_name = last_name
+
+        myuser.save()
+
+        messages.success(request, "Your Account has been successfully created.")
+
+        return redirect('sign-in')
+
     return render(request, 'authentication/signup.html')
 
 
